f17111d4af102c55e5dcf8d13fa7e126495e500d
Tiji Mathew <tiji.mathew@gmail.com>
2017-05-03 17:12:01 -0400

    Retreived the json contents into it's own div

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/3594343.json',
+  	dataType:'jsonp',
+  	success: function(response){
+  		var completedCourses = response.courses.completed;
+  		for (var i = 0; i < completedCourses.length; i++) {
+  			$('#badges').append('<div class="course">' + completedCourses[i].title + '</div>');
+  		}
+  	}
+  });
 
 });
